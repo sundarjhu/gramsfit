afe17a67738dda4487c31516850d82485b0d542f
Sundar Srinivasan <s.srinivasan@irya.unam.mx>
2023-07-18 23:52:20 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gramsfit',
+    version='1.0',
+    packages=find_packages(),
+    py_modules=[
+        'gramsfit',
+        'gramsfit_utils',
+        'gramsfit_nn',
+    ],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'astropy',
+        'torch',
+        'pyphot',
+        'h5py',
+        'sklearn',
+    ],
+    # entry_points={
+    #     'console_scripts': [
+    #         'your-script-name=your_package_name.module_name:main',
+    #     ],
+    # }
+)
